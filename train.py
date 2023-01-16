594da0721a110b5b9a74fd85cec269e40f542145
Akshay Vs <85959609+Akshay-Vs@users.noreply.github.com>
2023-01-16 07:50:46 +0530

    Initial commit

---
+++ train.py
@@ -0,0 +1,41 @@
+import cv2 as cv
+import numpy as np
+import matplotlib.pyplot as plt
+from tensorflow.keras import datasets, layers, models
+from tensorflow import keras
+import urllib
+
+# Data Preprocessing
+(training_images, training_labels), (testing_images, testing_labels) = datasets.cifar10.load_data()
+training_images, testing_images = training_images / 255, testing_images / 255
+
+class_names = ['Plane', 'Car', 'Bird', 'Cat', 'Deer', 'Dog', 'Frog', 'Horse', 'Ship', 'Truck']
+
+# Triming dataset
+training_images = training_images[:20000]
+training_labels = training_labels[:20000]
+testing_images = testing_images[:4000]
+testing_labels = testing_labels[:4000]
+
+# Model architecure
+model = models.Sequential()
+model.add(layers.Conv2D(32, (3,3), activation='relu', input_shape=(32,32,3)))
+model.add(layers.MaxPooling2D((2,2)))
+model.add(layers.Conv2D(64, (3,3), activation='relu'))
+model.add(layers.MaxPooling2D((2,2)))
+model.add(layers.Conv2D(64, (3,3), activation='relu'))
+model.add(layers.Flatten())
+model.add(layers.Dense(64, activation='relu'))
+model.add(layers.Dense(10, activation='softmax'))
+
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+
+# Train
+model.fit(training_images, training_labels, epochs=15, validation_data=(testing_images, testing_labels))
+
+# Evaluation
+loss, accuracy = model.evaluate(testing_images, testing_labels)
+print("Accuracy: ", accuracy *100)
+print("Loss: ", loss *100)
+
+model.save('image_classification.model')
